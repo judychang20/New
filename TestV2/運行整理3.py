2ea0c01549928d7288ddacc1953a0e67694b7d7b
Pinschen <51197029+Pinschen@users.noreply.github.com>
2019-06-07 09:23:58 +0800

    輪盤轉為鈴鐺更新2
    因輪盤畫面須與滑鼠操作相關才可運作，將輪盤畫面改為鈴鐺左右搖擺。
    問題:設定Time參數使最後結果顯現，但無回應，跑出libpng warning，故無法進入下頁

--- TestV2/運行整理3.py
+++ TestV2/運行整理3.py
@@ -226,23 +226,22 @@ def test_haveidea_page(x, y, z):
 
 def roulette_page(x, y, z):
 	font1 = pg.font.SysFont("myfont.ttf", 56)
-	Time = 0
-	if x < 500 and Time < 30:
+	Time = 1
+	if x < 500 and Time != 5:
 		Picture("第三頁背景.png", 0, 0, 1280, 720, bg).buildup()
-		Picture("左搖.png", 100, 100, 600, 600, bg).buildup()
-		Time += 5
-	elif x > 500 and Time < 30:
+		Picture("左搖.png", 100, 50, 700, 700, bg).buildup()
+		Time += 1
+	elif x > 500 and Time != 5:
 		Picture("第三頁背景.png", 0, 0, 1280, 720, bg).buildup()
-		Picture("右搖.png", 500, 100, 600, 600, bg).buildup()
-		Time += 5
-	elif Time == 30:
-		Picture("第三頁背景.png", 0, 0, 1280, x720, bg).buildup()
-		Picture("roulette.png", 50, 150, 1150, 1150, bg).buildup()
+		Picture("右搖.png", 500, 50, 700, 700, bg).buildup()
+		Time += 1
+	else :
+		Picture("第三頁背景.png", 0, 0, 1280, 720, bg).buildup()
+		Picture("左搖.png", 100, 50, 700, 700, bg).buildup()
 		Picture("視窗.png", 190, 150, 800, 400, bg).buildup()
 		result0 = "隨機結果"
 		result = Word(font1, result0, 560, 230, bg, (0, 0, 0)).buildup()
 		Picture("確定.png", 682, 348, 310, 110, bg).buildup()
-		d = Picture("確定.png", 682, 348, 310, 110, bg).buildup()
 
 
 def spotintro_page(x, y, z):
